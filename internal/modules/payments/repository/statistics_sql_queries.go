0d0f6cbfd7bd078b678d875f54ea1666d7e6976b
jumayevgadam <hypergadam@gmail.com>
2024-12-28 12:11:39 +0500

    statistics updated

--- internal/modules/payments/repository/statistics_sql_queries.go
+++ internal/modules/payments/repository/statistics_sql_queries.go
@@ -60,56 +60,35 @@ const (
 		WITH first_semester_payers AS (
 			SELECT student_id
 			FROM payments
-			WHERE payment_status = 'Accepted'
-				AND time_id = $1
-				AND payment_type = '1'  -- First semester payments
+			WHERE (payment_type = '1' OR payment_type = '3')
+			AND time_id = $1
 			GROUP BY student_id
-			HAVING COUNT(*) = (
-				SELECT COUNT(*)
-				FROM payments
-				WHERE student_id = payments.student_id
-					AND time_id = $1
-					AND payment_type = '1'
-					AND payment_status = 'Accepted'
-			)  -- Ensure all first semester payments are accepted
-		),
+			HAVING COUNT(*) = SUM(CASE WHEN payment_status = 'Accepted' THEN 1 ELSE 0 END)
+		),	
 		second_semester_payers AS (
 			SELECT student_id
 			FROM payments
-			WHERE payment_status = 'Accepted'
-				AND time_id = $1
-				AND payment_type = '2'  
+			WHERE (payment_type = '2' OR payment_type = '3')
+			AND time_id = $1
 			GROUP BY student_id
-			HAVING COUNT(*) = (
-				SELECT COUNT(*)
-				FROM payments
-				WHERE student_id = payments.student_id
-					AND time_id = $1
-					AND payment_type = '2'
-					AND payment_status = 'Accepted'
-			)  -- Ensure all second semester payments are accepted
+			HAVING COUNT(*) = SUM(CASE WHEN payment_status = 'Accepted' THEN 1 ELSE 0 END)
 		),
 		both_semester_payers AS (
 			SELECT student_id
-			FROM payments
-			WHERE payment_status = 'Accepted'
-				AND time_id = $1
-				AND payment_type IN ('1', '2')  
-			GROUP BY student_id
-			HAVING 
-				-- Ensure all payments for both semesters are accepted
-				student_id IN (SELECT student_id FROM first_semester_payers)
-				AND student_id IN (SELECT student_id FROM second_semester_payers)
+			FROM first_semester_payers
+			INTERSECT
+			SELECT student_id
+			FROM second_semester_payers
 		)
-		SELECT 
+		SELECT
 			(SELECT COUNT(*) FROM both_semester_payers) AS count_both_semester_paid,
-			(SELECT COUNT(*) 
-				FROM users
-				WHERE role_id = 3 
-					AND id NOT IN (
-						SELECT student_id
-						FROM both_semester_payers
-					)
+			(SELECT COUNT(*)
+			FROM users
+			WHERE role_id = 3
+			AND id NOT IN (
+				SELECT student_id
+				FROM both_semester_payers
+			)
 			) AS count_both_semester_not_paid;`
 
 	totalStudentQuery = `
@@ -144,58 +123,80 @@ const (
 				GROUP BY p.student_id
 				HAVING SUM(CASE WHEN p.payment_status = 'Accepted' THEN 1 ELSE 0 END) = COUNT(*)
 			)
-			) AS count_first_semester_not_paid;`
+		) AS count_first_semester_not_paid;`
 
 	secondSemesterStatisticsByFacultyQuery = `
 		SELECT 
-			(SELECT COUNT(DISTINCT p.student_id)
+		  (SELECT COUNT(*)
+			FROM (
+				SELECT p.student_id
 				FROM payments p
 				INNER JOIN users u ON u.id = p.student_id
-				INNER JOIN groups g ON u.group_id = g.id
-				WHERE g.faculty_id = $1 
-					AND p.payment_type IN ('2', '3') 
-					AND p.payment_status = 'Accepted' 
-					AND p.time_id = $2) AS count_second_semester_paid,
-			(SELECT COUNT(*) 
-				FROM users u
-				INNER JOIN groups g ON u.group_id = g.id
-				WHERE g.faculty_id = $1 
-				AND u.role_id = 3 
-				AND u.id NOT IN (
-					SELECT DISTINCT p.student_id
-					FROM payments p
-					WHERE p.payment_type IN ('2', '3') 
-						AND p.payment_status = 'Accepted' 
-						AND p.time_id = $2
-			)) AS count_second_semester_not_paid;`
+				INNER JOIN "groups" g ON g.id = u.group_id
+				WHERE p.payment_type IN ('2', '3') 
+				AND g.faculty_id = $1
+				AND p.time_id = $2
+				GROUP BY p.student_id
+				HAVING SUM(CASE WHEN p.payment_status = 'Accepted' THEN 1 ELSE 0 END) = COUNT(*)
+			) AS paid_students
+			) AS count_first_semester_paid,
+		  (SELECT COUNT(*)
+			FROM users u INNER JOIN "groups" g2 ON g2.id = u.group_id
+			WHERE u.role_id = 3 AND g2.faculty_id = $1
+			AND u.id NOT IN (
+				SELECT p.student_id
+				FROM payments p INNER JOIN users u ON u.id = p.student_id
+				INNER JOIN "groups" g3 ON g3.id = u.group_id
+				WHERE p.payment_type IN ('2', '3') 
+					AND g3.faculty_id = $1
+					AND p.time_id = $2
+				GROUP BY p.student_id
+				HAVING SUM(CASE WHEN p.payment_status = 'Accepted' THEN 1 ELSE 0 END) = COUNT(*)
+			)
+		  ) AS count_first_semester_not_paid;`
 
 	bothSemesterStatisticsByFacultyQuery = `
-		WITH both_semester_payers AS (
-			SELECT p.student_id
-			FROM payments p
-			INNER JOIN users u ON u.id = p.student_id
-			INNER JOIN groups g ON u.group_id = g.id
-			WHERE g.faculty_id = $1 
-			  AND p.payment_status = 'Accepted'
+		  WITH first_semester_payers AS (
+			  SELECT p.student_id
+			  FROM payments p
+			  INNER JOIN users u ON u.id = p.student_id
+			  INNER JOIN groups g ON u.group_id = g.id
+			  WHERE g.faculty_id = $1
 			  AND p.time_id = $2
-			  AND p.payment_type IN ('1', '2', '3')
-			GROUP BY p.student_id
-			HAVING 
-				COUNT(DISTINCT p.payment_type) = 2 OR 
-				MAX(p.payment_type) = '3'
-		)
-		SELECT 
-			(SELECT COUNT(*) FROM both_semester_payers) AS count_both_semester_paid,
-			(SELECT COUNT(*) 
-			 FROM users u
-			 INNER JOIN groups g ON u.group_id = g.id
-			 WHERE g.faculty_id = $1 
+			  AND p.payment_type IN ('1', '3')
+			  GROUP BY p.student_id
+			  HAVING COUNT(*) = SUM(CASE WHEN p.payment_status = 'Accepted' THEN 1 ELSE 0 END)
+		  ),
+		  second_semester_payers AS (
+			  SELECT p.student_id
+			  FROM payments p
+			  INNER JOIN users u ON u.id = p.student_id
+			  INNER JOIN groups g ON u.group_id = g.id
+			  WHERE g.faculty_id = $1
+			  AND p.time_id = $2
+			  AND p.payment_type IN ('2', '3')
+			  GROUP BY p.student_id
+			  HAVING COUNT(*) = SUM(CASE WHEN p.payment_status = 'Accepted' THEN 1 ELSE 0 END)
+		  ),
+		  both_semester_payers AS (
+			  SELECT student_id
+			  FROM first_semester_payers
+			  INTERSECT
+			  SELECT student_id
+			  FROM second_semester_payers
+		  )
+		  SELECT 
+			  (SELECT COUNT(*) FROM both_semester_payers) AS count_both_semester_paid,
+			  (SELECT COUNT(*) 
+			   FROM users u
+			   INNER JOIN groups g ON u.group_id = g.id
+			   WHERE g.faculty_id = $1 
 			   AND u.role_id = 3 
 			   AND u.id NOT IN (
 				   SELECT student_id
 				   FROM both_semester_payers
-			   )) AS count_both_semester_not_paid;
-		`
+			   )
+			  ) AS count_both_semester_not_paid;`
 
 	totalStudentsCountByFacultyQuery = `
 		SELECT COUNT(u.id) 
